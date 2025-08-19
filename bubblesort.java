40143c08d2b696d7be8e76da7a6107877ef27b42
CHIMMALARUTHIKSIVA <cruthikshiva@gmail.com>
2025-08-19 14:06:21 +0530

    Add files via upload

--- bubblesort.java
+++ bubblesort.java
@@ -1,29 +1,34 @@
-import java.util.Scanner;
-class bubblesort {
-    public static void main(String[] args) {
-       // Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int size = arr.length;
-        int temp;
-        System.out.println("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Vishnu");
-        for (int i=0;i<size-1;i++) {
-            for (int j=0;j<size-1-i;j++) {
-                if (arr[j]>arr[j+1]) {
-                    temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+package sorting;
+
+class BubbleSort {
+    void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-        }
-        for (int i=0;i<size;i++) {
-            System.out.println(arr[i]);
-        }
+    }
+
+    // Utility method to print the array
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array:");
+        ob.printArray(arr);
     }
 }
+
+
+
